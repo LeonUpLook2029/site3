5430e782fb70440a0ee5c01d40b26aa0c1fd48ea
LeonUpLook <pkcet2017@gmail.com>
2022-09-16 22:21:27 +0300

    start main in HTTP "about-as"

--- website3/main/urls.py
+++ website3/main/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('about-as', views.about)
 ]
--- website3/main/views.py
+++ website3/main/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(requst):
     return HttpResponse('<h4>Hello!</h4>')
+
+def about(requst):
+    return HttpResponse('<h2>About us</h2>')
