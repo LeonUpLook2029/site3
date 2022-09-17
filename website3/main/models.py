5589668ad5f51fdcc6717b60115be5fe6be67c9f
LeonUpLook <pkcet2017@gmail.com>
2022-09-17 22:23:44 +0300

    start main HTML migrate

--- website3/main/models.py
+++ website3/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField('Название',max_length=50)
+    task = models.TextField('Описание')
+    user = models.CharField('Пользователь',max_length=100)
+    date = models.DateField('Дата')
+
+    def __str__(self):
+        return self.title
