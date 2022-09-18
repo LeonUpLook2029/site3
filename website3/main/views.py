577b424721ff86cbe47aa4750099d9e8a695dc36
LeonUpLook <pkcet2017@gmail.com>
2022-09-18 13:03:11 +0300

    start main HTML task = Task.objects.all() in index.html

--- website3/main/views.py
+++ website3/main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Task
+# не требуется т.к. все обьекты в словарь
+# from .models import User
+# from .models import Date
 
 def index(requst):
-    return render(requst, 'main/index.html')
-
+    task = Task.objects.all()
+    # user = User.objects.all() не требуется т.к. все обьекты в словарь
+    # date = Date.objects.all()
+    return render(requst, 'main/index.html', {'title': 'Главная страница', 'tasks': task})
+# {, 'users': user, 'dates': date} не требуется т.к. все обьекты в словарь 'tasks'
 def about(requst):
     return render(requst, 'main/about.html')
 
