9567bb839913ec523eaece77f91ea1004167d736
LeonUpLook <pkcet2017@gmail.com>
2022-09-21 22:26:19 +0300

    start main forms.py widgets = {"title": TextInput(attrs={'class':'from-control', 'placeholder':'Введите название'}),

--- website3/main/forms.py
+++ website3/main/forms.py
@@ -1,7 +1,22 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, DateInput
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ["title", "task", "user", "date"]
+        widgets = {"title": TextInput(attrs={'class':'from-control', 'placeholder':'Введите название'}),
+            "task": TextInput(attrs={
+                'class': 'from-control',
+                'placeholder': 'Введите описание'
+            }),
+            "user": TextInput(attrs={
+                'class': 'from-control',
+                'placeholder': 'Введите автора'
+            }),
+            "date": DateInput(attrs={
+                'class': 'from-control',
+                'placeholder': 'Введите дату'
+            }),
+
+        }
